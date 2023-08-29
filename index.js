e78f5fc9e689b6680ee3cdcc0c86bc73d43cc01b
Junel <robin.lennuj@gmail.com>
2023-08-29 15:50:19 -0700

    Progress on Prompts for index.js.

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.use('/api', routes);
-
-
 // Add Prompt for menu.
 async function promptMenu() {
     const answers = await inquirer.prompt([
@@ -36,39 +33,96 @@ async function promptMenu() {
 
 
 // Create If statements for "View all departments."
+if (answers.menu === "View all departments.") {
+
+// Call a function to view all departments.
+
+}
+
 
 
 
 // Create If statements for "View all roles."
+if (answers.menu === "View all roles.") {
+
+// Call a function to View all roles.
+    
+}
+
 
 
 
 // Create If statements for "View all employees."
+if (answers.menu === "View all employees.") {
+
+// Call a function to View all employees.
+        
+}
+
 
 
 
 // Create If statements for "Add new department."
 // Create Prompt for input.
+if (answers.menu === "Add new department.") {
+    const answers = await inquirer.prompt([
+// Call a function to add new department.
+        {
+
+        }]);
+        
+}
+
 
 
 
 // Create If statements for "Add new role."
 // Create Prompt for input.
+if (answers.menu === "Add new role.") {
+    const answers = await inquirer.prompt([
+// Call a function to add new role.
+        {
+        
+        }]);
+                
+}
+
 
 
 
 // Create If statements for "Add new employee."
 // Create Prompt for input.
+if (answers.menu === "Add new employee.") {
+    const answers = await inquirer.prompt([
+// Call a function to add new employee.
+        {
+                
+        }]);
+                        
+}
+
 
 
 
 // Create If statements for "Update employee role."
 // Create Prompt for input.
+if (answers.menu === "Update employee role.") {
+    const answers = await inquirer.prompt([
+// Call a function to update employee role.
+        {
+                
+        }]);
+                        
+}
 
 
 
+// Exiting the application
+else if (answers.menu === "Exit") {
+
+console.log("Exiting the application...");
+    process.exit(0);
+}
 
 
-app.listen(port, () => {
-    console.log(`server running on port ${port}`);
-});
+promptMenu();
