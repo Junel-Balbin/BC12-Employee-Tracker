22ab4d826d2bdf368fd0ba5bda393b920f03d355
Junel <robin.lennuj@gmail.com>
2023-08-29 15:17:03 -0700

    Made changes to the schema.sql & seeds.sql.

--- db/schema.sql
+++ db/schema.sql
@@ -5,29 +5,25 @@ USE employeeTracker_db;
 
 CREATE TABLE  department (
     id INT NOT NULL auto_increment,
-    department_name VARCHAR(100) not null,
+    department_name VARCHAR(30) not null,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE manager (
-    id INT NOT NULL auto_increment,
-    department_name VARCHAR(100) not null,
-    PRIMARY KEY(id)
-);
-
-CREATE TABLE employee (
-    id INT NOT NULL auto_increment,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    role_id INT,
-    manager_name VARCHAR(100),
-    PRIMARY KEY(id)
-);
 
 CREATE TABLE role (
     id INT NOT NULL auto_increment,
-    title VARCHAR(100),
+    title VARCHAR(30),
     salary DECIMAL,
-    department_name VARCHAR(100),
+    department_id INT,
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE employee (
+    id INT NOT NULL auto_increment,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
     PRIMARY KEY(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,30 +1,26 @@
 INSERT INTO department (department_name) VALUES
     ("Human Resources"),
     ("Finance Billing"),
-    ("Engineering"),
-    ("Maintenance"),
-    ("Production"),
-    ("Cafeteria");
+    ("Engineering Production");
 
-INSERT INTO manager (manager_name, department_name, salary)
-    ("Clark Kent", "Finance Billing", 50000),
-    ("Diana Prince", "Human Resources", 50000),
-    ("Bruce Wayne", "Engineering", 50000);
+INSERT INTO role (title, salary, department_id) VALUES
+    ("Recruiter Specialist", 30000, 1),
+    ("Senior Recruiter", 40000, 1),
+    ("Associate Accountant", 30000, 2),
+    ("Senior Accountant", 40000, 2),
+    ("Associate Engineer", 30000, 3),
+    ("Senior Engineer", 40000, 3),
+    ("HR Manager", 50000, 1),
+    ("FB Manager", 50000, 2),
+    ("EP Manager", 50000, 3);
 
-
-INSERT INTO employee (first_name, last_name, role_id, manager_name)
-    ("Dick", "Grayson", 4, "Bruce Wayne"),
-    ("Jimmy", "Olsen", 5, "Clark Kent"),
-    ("Etta", "Candy", 6, "Diana Prince");
-    ("Clark", "Kent", 1, NULL),
-    ("Diana", "Prince", 2, NULL),
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+    ("Etta", "Candy", 4, 1),
+    ("Barbara", "Minerva", 5, 1),
+    ("Lois", "Lane", 6, 2),
+    ("Jimmy", "Olsen", 7, 2),
+    ("Dick", "Grayson", 8, 3),
+    ("Terry", "McGinnis", 9, 3),
+    ("Diana", "Prince", 1, NULL),
+    ("Clark", "Kent", 2, NULL),
     ("Bruce", "Wayne", 3, NULL);
-
-
-INSERT INTO role (title, salary, department_name)
-    ("Associate Engineer", 40000, "Engineering"),
-    ("Associate Accountant", 40000, "Finance Billing"),
-    ("Recruiter Specialist", 40000, "Human Resources"),
-    ("Manager", 50000, "Finance Billing"),
-    ("Manager", 50000, " Human Resources"),
-    ("Manager", 50000, "Engineering");
