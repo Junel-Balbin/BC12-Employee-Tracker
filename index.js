2000c5a91d01776e84ab08e6bcc4f1b7f6615b35
Junel <robin.lennuj@gmail.com>
2023-08-29 12:04:03 -0700

    Started working on Prompt functions.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const routes = require("./routes");
+const { default: inquirer } = require('inquirer');
 
 const port = process.env.PORT || 3001;
 
@@ -11,6 +12,62 @@ app.use(express.json());
 
 
 app.use('/api', routes);
+
+
+// Add Prompt for menu.
+async function promptMenu() {
+    const answers = await inquirer.prompt([
+        {
+            type: "list",
+            name: "menu",
+            message: "How can I assist you today?",
+            choices: [
+                "View all departments.",
+                "View all roles.",
+                "View all employees.",
+                "Add new department.",
+                "Add new role.",
+                "Add new employee.",
+                "Update employee role."
+            ]
+        }
+    ]);
+}
+
+
+// Create If statements for "View all departments."
+
+
+
+// Create If statements for "View all roles."
+
+
+
+// Create If statements for "View all employees."
+
+
+
+// Create If statements for "Add new department."
+// Create Prompt for input.
+
+
+
+// Create If statements for "Add new role."
+// Create Prompt for input.
+
+
+
+// Create If statements for "Add new employee."
+// Create Prompt for input.
+
+
+
+// Create If statements for "Update employee role."
+// Create Prompt for input.
+
+
+
+
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
