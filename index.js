d2c8cae40e2150468a55f9a9fcbb7abf507c0310
Junel <robin.lennuj@gmail.com>
2023-08-30 13:23:47 -0700

    Completed "Add & Update" prompts.

--- index.js
+++ index.js
@@ -72,71 +72,127 @@ async function promptMenu() {
         });
     }
 
-/*
-
-
-// Create If statements for "Add new department."
-// Create Prompt for input.
-if (answers.menu === "Add new department.") {
-    const answers = await inquirer.prompt([
-// Call a function to add new department.
-        {
-
-        }]);
+    // Create If statements for "Add new department."
+    // Create Prompt for input.
+    if (answers.menu === "Add new department.") {
+        const answers = await inquirer.prompt([
+            {
+                type: "input",
+                name: "newDepartment",
+                message: "Type new department name."
+            }
+        ]);
         
-}
-
-
-
-
-// Create If statements for "Add new role."
-// Create Prompt for input.
-if (answers.menu === "Add new role.") {
-    const answers = await inquirer.prompt([
-// Call a function to add new role.
-        {
-        
-        }]);
-                
-}
-
-
-
-
-// Create If statements for "Add new employee."
-// Create Prompt for input.
-if (answers.menu === "Add new employee.") {
-    const answers = await inquirer.prompt([
-// Call a function to add new employee.
-        {
-                
-        }]);
-                        
-}
-
-
-
-
-// Create If statements for "Update employee role."
-// Create Prompt for input.
-if (answers.menu === "Update employee role.") {
-    const answers = await inquirer.prompt([
-// Call a function to update employee role.
-        {
-                
-        }]);
-                        
-}
-
-*/
-
-
-// Exiting the application
-else if (answers.menu === "Exit") {
-console.log("Have an AWESOME day!");
-        process.exit(0);
+        db.query(`INSERT INTO department (department_name) VALUES (?)`, [answers.newDepartment], (err, result) => {
+            if (err) {
+                console.error("SQL query error:", err);
+            } else {
+                promptMenu();
+            }
+        });
     }
-}
+    
+    // Create If statements for "Add new role."
+    // Create Prompt for input.
+    if (answers.menu === "Add new role.") {
+        const answers = await inquirer.prompt([
+            {
+                type: "input",
+                name: "newRole",
+                message: "Type new Role."
+            },
+            {
+                type: "input",
+                name: "newSalary",
+                message: "Type Salary."
+            },
+            {
+                type: "input",
+                name: "newDepartmentID",
+                message: "Type Department ID."
+            }
+        ]);
+        
+        db.query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, [answers.newRole, answers.newSalary, answers.newDepartmentID], (err, result) => {
+            if (err) {
+                console.error("SQL query error:", err);
+            } else {
+                promptMenu();
+            }
+        });
+    }
+
+    // Create If statements for "Add new employee."
+    // Create Prompt for input.
+    if (answers.menu === "Add new employee.") {
+        const answers = await inquirer.prompt([
+            {
+                type: "input",
+                name: "firstName",
+                message: "Type First Name."
+            },
+            {
+                type: "input",
+                name: "lastName",
+                message: "Type Last Name."
+            },
+            {
+                type: "input",
+                name: "roleID",
+                message: "Type Role ID."
+            },
+            {
+                type: "input",
+                name: "managerID",
+                message: "Type Manager ID."
+            }
+        ]);
+        
+        db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [answers.firstName, answers.lastName, answers.roleID, answers.managerID], (err, result) => {
+            if (err) {
+                console.error("SQL query error:", err);
+            } else {
+                promptMenu();
+            }
+        });
+    }
+
+
+    // Create If statements for "Update employee role."
+    // Create Prompt for input.
+    if (answers.menu === "Update employee role.") {
+        const updateAnswers = await inquirer.prompt([
+            {
+                type: "input",
+                name: "employeeName",
+                message: "Enter the employee's name: "
+            },
+            {
+                type: "input",
+                name: "newRoleID",
+                message: "Enter new role ID: "
+            }
+        ]);
+
+        db.query(
+            "UPDATE employee SET role_id = ? WHERE first_name = ? OR last_name = ?",
+            [updateAnswers.newRoleID, updateAnswers.employeeName, updateAnswers.employeeName],
+            (err, result) => {
+                if (err) {
+                    console.error("SQL query error:", err);
+                }
+                promptMenu();
+            }
+        );
+    }
+
+
+    // Exiting the application
+    else if (answers.menu === "Exit") {
+        console.log("Have an AWESOME day!");
+            process.exit(0);
+        }
+    }
 
 
 promptMenu();
